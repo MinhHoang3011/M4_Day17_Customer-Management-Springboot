6252575c915a6caf94553429d7f635a991e12daf
MinhHoang3011 <minhhoang301198@gmail.com>
2022-09-28 16:19:28 +0700

    done

---
+++ src/main/java/com/example/customermanagerspringboot/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.example.customermanagerspringboot.service;
+
+import com.example.customermanagerspringboot.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer>{
+}
